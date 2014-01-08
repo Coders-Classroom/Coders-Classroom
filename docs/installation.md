c29f1dd7fb6d6272ca05a186e534a394583b44b9
Glavin Wiechert <Glavin.Wiechert@gmail.com>
2014-01-08 00:40:57 -0400

    Worked on #12.

---
+++ docs/installation.md
@@ -0,0 +1,13 @@
+# Installation
+
+We have written a simple Node.js installer script.
+It will hwalk you through installation, including but not limited to: most common troubleshooting problems, configuration, and running any build commands, ending with a brief usage tutorial.
+
+## Installer
+
+Run the Node.js script:
+
+```bash
+node install.js
+```
+
--- install.js
+++ install.js
@@ -1,10 +1,21 @@
 
 // Dependencies
-var prompt = require("prompt");
+var sys = require("sys");
+var exec = require("child_process").exec;
+//var prompt = require("prompt");
+var yesno = require('yesno');
+var pkg = require("./package.json");
+
+// Display Banner
+console.log();
+console.log("=====");
+console.log("===== "+pkg.name+", "+pkg.version);
+console.log("===== "+pkg.description);
+console.log("=====");
+console.log();
 
 // Start Prompt
 prompt.start();
-console.log();
 
 // Helpers
 function hasWhiteSpace(s) {
@@ -12,11 +23,55 @@ function hasWhiteSpace(s) {
 }
 
 // Check for White Space in Path, Issue #11.
-if ( hasWhiteSpace(__dirname) ) {
+if ( 
+        !require('nodegit') && // Checking if nodegit is successfully installed
+        hasWhiteSpace(__dirname) // if nodegit is not installed, verify the installation path does not contain spaces
+        ) {
     console.error("Your installation directory has white space characters.");
     console.log("See issue at https://github.com/Glavin001/Coders-Classroom/issues/11");
     console.log("We recommend changing your path from '"+__dirname+"' to '"+__dirname.replace(/\s/g, "_")+"' that users '_' underscoes instead of spaces.");
+    console.log();
 
+    process.exit(1);
 }
 
+// NPM Install
+var npmInstall = function(package, callback) {
+    exec('npm install '+(package||""), function(error, stdout, stderr) {
+        callback(error,stdout,stderr);
+    });
+};
+
+// Bower Install
+var bowerInstall = function(package, callback) {
+    exec('bower install '+(package||""), function(error, stdout, stderr) {
+        callback(error,stdout,stderr);
+    });
+};
+
+// Install Dependencies
+yesno.ask("Would you like to install all node dependencies with 'npm install'?", true, function(npmOk) {
+    yesno.ask("Would you like to install all bower dependencies with 'npm install'?", true, function(bowerOk) {
+        if (npmOk) {
+            npmInstall(null, function(error, stdout, stderr) {
+                if (error) {
+                    console.log(error, stderr);
+                } else {
+                    console.log("NPM successfully installed dependencies.");
+                }
+            });
+        }
+        if (bowerOk) {
+            bowerInstall(null, function(error, stdout, stderr) {
+                if (error) {
+                    console.log(error, stderr);
+                } else {
+                    console.log("Bower successfully installed dependencies.");
+                }
+            });
+        }
+    });
+});
+
 console.log();
+
